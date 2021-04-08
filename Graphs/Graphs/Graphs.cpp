a6bce456fa740e07aa319fe4e5793d2c0da06c72
MyxaWanyuha <sablin.2001@inbox.ru>
2021-04-08 20:39:13 +0500

    d

--- Graphs/Graphs/Graphs.cpp
+++ Graphs/Graphs/Graphs.cpp
@@ -7,27 +7,15 @@
 class Graph
 {
 public:
-    Graph( std::vector<std::pair<int, int>> V)
-    {
-        VertexCount = FindVertexCount(V);
-        adjLists.resize(VertexCount);
-        num.resize(VertexCount, -1);
-        visited.resize(VertexCount, 0);
-        father.resize(VertexCount, -1);
-        for (auto& edge : V)
-            adjLists[edge.first].push_back(edge.second);
-        for (auto& e : adjLists)
-            std::sort(e.begin(), e.end());
-    }
-
-    void DFS(std::vector<std::pair<int, int>>& T,
+    void DFS(
+        std::vector<std::pair<int, int>>& T,
         std::vector<std::pair<int, int>>& B,
-        int& i, int v)
+        int& i, 
+        int v)
     {
         num[v] = i++;
-        for (auto u : adjLists[v])
+        for (int u : adjLists[v])
         {
-            visited[v]++;
             if (num[u] == -1)
             {
                 T.push_back({ v, u });
@@ -39,8 +27,19 @@ public:
         }
     }
 
+    Graph(std::vector<std::pair<int, int>> V)
+    {
+        VertexCount = FindVertexCount(V);
+        adjLists.resize(VertexCount);
+        num.resize(VertexCount, -1);
+        father.resize(VertexCount, -1);
+        for (auto& edge : V)
+            adjLists[edge.first].push_back(edge.second);
+        for (auto& e : adjLists)
+            std::sort(e.begin(), e.end());
+    }
+
     int GetVertexCount() const { return VertexCount; }
-    int IsVisitedVertex(int v) { return visited[v] == adjLists[v].size(); }
 private:
     int FindVertexCount(std::vector<std::pair<int, int>>& V) const
     {
@@ -51,12 +50,10 @@ private:
             if (eMax > res)
                 res = eMax;
         }
-
         return res + 1;
     }
     int VertexCount;
     std::vector<std::vector<int>> adjLists;
-    std::vector<int> visited;
     std::vector<int> num;
     std::vector<int> father;
 };
@@ -64,36 +61,30 @@ private:
 std::vector<std::pair<int, int>> ParseInput(std::string& input)
 {
     std::vector<std::pair<int, int>> res;
-
-    //delete '[' and ']'
-    input = input.substr(1);
-    input.erase(input.end() - 1);
-
-    for (int i = 0; i < input.size(); i++)
+    for (int i = 1; i < input.size(); i++)
     {
-        std::pair<int, int> pairRes;
-
-        std::string digit;
         if (input[i] == '(')
         {
+            std::string number;
             i++;
             //read first digit
             while (input[i] != ',')
             {
-                digit += input[i];
+                number += input[i];
                 i++;
             }
-            pairRes.first = std::stoi(digit);
+            std::pair<int, int> pairRes;
+            pairRes.first = std::stoi(number);
 
             i += 2; //skip ", "
-            digit = "";
+            number = "";
             //read second digit
             while (input[i] != ')')
             {
-                digit += input[i];
+                number += input[i];
                 i++;
             }
-            pairRes.second = std::stoi(digit);
+            pairRes.second = std::stoi(number);
             res.push_back(pairRes);
         }
     }
@@ -118,7 +109,12 @@ void OutputToFile(std::string fileName, std::vector<std::pair<int, int>>& v)
 int main()
 {
     {
-        std::ifstream t("graph1.txt");
+        std::string GraphFileName = "graph2.txt";
+        std::string OutputTFileName = "T" + GraphFileName.substr(5);
+        std::string OutputBFileName = "B" + GraphFileName.substr(5);
+
+        std::ifstream t(GraphFileName);
+        
         std::string input;
         t.seekg(0, std::ios::end);
         input.reserve(t.tellg());
@@ -126,7 +122,6 @@ int main()
         input.assign((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
 
         auto V = ParseInput(input);
-        //V = { {0, 1}, {0, 6}, {1, 2}, {1, 4}, {1, 5}, {2, 3}, {2, 4}, {3, 4}, {5, 6}, {5, 7}, {5, 8}, {7, 8} };
         Graph g(V);
 
         std::vector<std::pair<int, int>> T;
@@ -134,29 +129,15 @@ int main()
         int i = 0;
 
         for (int j = 0; j < g.GetVertexCount(); ++j)
-            if(!g.IsVisitedVertex(j))
-                g.DFS(T, B, i, j);
-
-        bool isIncoherent = true;
-        for (int j = 0; j < g.GetVertexCount(); ++j)
-            if (g.IsVisitedVertex(j) != 1)
-            {
-                isIncoherent = false;
-                break;
-            }
-
-        if(isIncoherent)
-            std::cout << "incoherent\n";
-        else
-            std::cout << "not incoherent\n";
+           g.DFS(T, B, i, j);
 
         if (B.size() == 0)
             std::cout << "Is tree\n";
         else
             std::cout << "Is not tree\n";
 
-        OutputToFile("T.txt", T);
-        OutputToFile("B.txt", B);
+        OutputToFile(OutputTFileName, T);
+        OutputToFile(OutputBFileName, B);
     }
 
     return 0;
